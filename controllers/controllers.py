59a3e14aa2ccb6e71d05c2977c26567b144b5b1d
alvin.z <alvin.z.ca@gmail.com>
2022-10-04 12:11:51 -0600

    调试

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from odoo import http
-
+import json
 
 class Purchasecard(http.Controller):
     @http.route('/purchasecard/purchasecard', auth='public')
@@ -9,6 +9,13 @@ class Purchasecard(http.Controller):
 
     @http.route('/purchasecard/purchasecard/print/<uuid>', auth='public')
     def list(self, uuid, **kw):
+        purchasecard = http.request.env['st.purchasecard'].search([('uuid', '=', uuid)], limit=1)
+        if not purchasecard:
+            raise GraphQLError(_('Purchase Card does not exist.'))
+        
+        purchaseCardGrid = json.loads(purchasecard['data'])  
+        
         return http.request.render('purchasecard.print', {
             'uuid': uuid,
+            'data': purchasecard['data']
         })
